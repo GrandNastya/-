58f9e996d7518533061179eb9887154ede35988c
Nastya <90123161+GrandNastya@users.noreply.github.com>
2022-01-30 13:25:50 +0700

    Add files via upload

---
+++ hh/level.py
@@ -0,0 +1,6 @@
+import pygame.image
+
+class Level:
+    def __init__(self):
+        self.value = 1
+        self.image = pygame.image.load(r'img/')
---
+++ hh/name.py
@@ -0,0 +1,4 @@
+class Name:
+    def __init__(self):
+        self.image = pygame.image.load('img/name.png')
+        self.pos = (650, 0)
